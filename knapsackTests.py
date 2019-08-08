d69b1c997b131819f28b3dd41dad510a1aff757a
Aleksander Los <oleklos@gmail.com>
2019-08-08 16:09:16 +0200

    Finish basic Knapsack problem

--- knapsackTests.py
+++ knapsackTests.py
@@ -11,9 +11,9 @@ def get_fitness(genes):
     totalValue = 0
     for iq in genes:
         count = iq.Quantity
-        totalWeight += iq.Weight * count
-        totalVolume += iq.Volume * count
-        totalValue += iq.Value * count
+        totalWeight += iq.Item.Weight * count
+        totalVolume += iq.Item.Volume * count
+        totalValue += iq.Item.Value * count
 
     return Fitness(totalWeight, totalVolume, totalValue)
 
@@ -54,7 +54,7 @@ def mutate(genes, items, maxWeight, maxVolume):
 
     removing = len(genes) > 1 and random.randint(0, 10) == 0
     if removing:
-        index = randrange(0, len(genes))
+        index = random.randrange(0, len(genes))
         iq = genes[index]
         item = iq.Item
         remainingWeight += item.Weight * iq.Quantity
@@ -76,6 +76,30 @@ def mutate(genes, items, maxWeight, maxVolume):
     remainingWeight += item.Weight * iq.Quantity
     remainingVolume += item.Volume * iq.Quantity
 
+    changeItem = len(genes) < len(items) and random.randint(0, 4) == 0
+    if changeItem:
+        itema, itemb = random.sample(items, 2)
+        item = itema if itema != item else itemb
+
+    maxQuantity = max_quantity(item, remainingWeight, remainingVolume)
+    if maxQuantity > 0:
+        quantity = random.randint(1, maxQuantity)
+        genes[index] = ItemQuantity(item, quantity)
+    else:
+        del genes[index]
+
+def display(candidate, startTime):
+    timeDiff = datetime.datetime.now() - startTime
+    genes = candidate.Genes[:]
+    genes.sort(key=lambda iq: iq.Quantity, reverse=True)
+
+    descriptions = [str(iq.Quantity) + "x" + iq.Item.Name for iq in genes]
+    if len(descriptions) == 0:
+        descriptions.append("Empty")
+    print("{}\t{}\t{}".format(
+        ', '.join(descriptions),
+        candidate.Fitness,
+        timeDiff))
 
 class Resource:
     def __init__(self, name, value, weight, volume):
@@ -85,18 +109,9 @@ class Resource:
         self.Volume = volume
 
 
-class KnapsackTests(unittest.TestCase):
-    def test_cookies(self):
-        items = [
-                Resource("Flour", 1680, 0.265, .41),
-                Resource("Butter", 1440, 0.5, .13),
-                Resource("Sugar", 1840, 0.441, .29)
-            ]
-
-
 class ItemQuantity:
     def __init__(self, item, quantity):
-        self.Item = items
+        self.Item = item
         self.Quantity = quantity
 
     def __eq__(self, other):
@@ -111,7 +126,7 @@ class Fitness:
 
     def __gt__(self, other):
         if self.TotalValue != other.TotalValue:
-            return self.TotalValue > other.totalValue
+            return self.TotalValue > other.TotalValue
         if self.TotalWeight != other.TotalWeight:
             return self.TotalWeight < other.TotalWeight
         return self.TotalVolume < self.TotalVolume
@@ -121,3 +136,41 @@ class Fitness:
                 self.TotalWeight,
                 self.TotalVolume,
                 self.TotalValue)
+
+
+class KnapsackTests(unittest.TestCase):
+    def test_cookies(self):
+        items = [
+                Resource("Flour", 1680, 0.265, .41),
+                Resource("Butter", 1440, 0.5, .13),
+                Resource("Sugar", 1840, 0.441, .29)
+            ]
+        maxWeight = 10
+        maxVolume = 4
+        optimal = get_fitness([ItemQuantity(items[0], 1),
+                               ItemQuantity(items[1], 14),
+                               ItemQuantity(items[2], 6)])
+        self.fill_knapsack(items, maxWeight, maxVolume, optimal)
+
+    def fill_knapsack(self, items, maxWeight, maxVolume, optimalFitness):
+        startTime = datetime.datetime.now()
+
+        def fnDisplay(candidate):
+            display(candidate, startTime)
+
+        def fnGetFitness(genes):
+            return get_fitness(genes)
+
+        def fnCreate():
+            return create(items, maxWeight, maxVolume)
+
+        def fnMutate(genes):
+            mutate(genes, items, maxWeight, maxVolume)
+
+        best = genetic.get_best(fnGetFitness, None, optimalFitness, None,
+                                fnDisplay, fnMutate, fnCreate, maxAge=50)
+        self.assertTrue(not optimalFitness > best.Fitness)
+
+
+if __name__ == '__main__':
+    unittest.main()
