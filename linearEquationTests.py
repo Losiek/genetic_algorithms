cc1b3981542034821d30873befc55fe09aa815b9
Aleksander Los <oleklos@gmail.com>
2019-08-15 20:36:45 +0200

    Refactor for 3 unknows in equation

--- linearEquationTests.py
+++ linearEquationTests.py
@@ -1,23 +1,23 @@
 import unittest
 import datetime
+import fractions
+
 import genetic
 
 
-def get_fitness(genes):
-    x, y = genes[0:2]
-
-    e1 = x + 2 * y - 4
-    e2 = 4 * x + 4 * y - 12
-    fitness = Fitness(abs(e1) + abs(e2))
-
+def get_fitness(genes, equations):
+    fitness = Fitness(sum(abs(e(genes)) for e in equations))
     return fitness
 
 
-def display(candidate, startTime):
+def display(candidate, startTime, fnGenesToInputs):
     timeDiff = datetime.datetime.now() - startTime
-    x, y = candidate.Genes[0:2]
-    print("x = {}, y = {}\t{}\t{}".format(
-        x, y, candidate.Fitness, timeDiff))
+    symbols = "xyz"
+    result = ", ".join(
+        "{} = {}".format(s, v)
+        for s, v in zip(symbols, fnGenesToInputs(candidate.Genes))
+    )
+    print("{}\t{}\t{}".format(result, candidate.Fitness, timeDiff))
 
 
 class Fitness:
@@ -32,20 +32,70 @@ class Fitness:
 
 
 class LinearEquationTests(unittest.TestCase):
-    def test(self):
-        geneset = [i for i in range(10)]
+    def solve_unknowns(self, numUnknows, geneset, equations, fnGenesToInputs):
         startTime = datetime.datetime.now()
 
         def fnDisplay(candidate):
-            display(candidate, startTime)
+            display(candidate, startTime, fnGenesToInputs)
 
         def fnGetFitness(genes):
-            return get_fitness(genes)
+            return get_fitness(genes, equations)
 
         optimalFitness = Fitness(0)
-        best = genetic.get_best(fnGetFitness, 2, optimalFitness,
-                                geneset, fnDisplay, maxAge=50)
+        best = genetic.get_best(
+            fnGetFitness,
+            numUnknows,
+            optimalFitness,
+            geneset,
+            fnDisplay,
+            maxAge=50,
+        )
         self.assertTrue(not optimalFitness > best.Fitness)
+
+    def test_2_unknowns(self):
+        geneset = [i for i in range(-5, 5) if i != 0]
+
+        def fnGenesToInputs(genes):
+            return genes[0], genes[1]
+
+        def e1(genes):
+            x, y = fnGenesToInputs(genes)
+            return x + 2 * y - 4
+
+        def e2(genes):
+            x, y = fnGenesToInputs(genes)
+            return 4 * x + 4 * y - 12
+
+        equations = [e1, e2]
+        self.solve_unknowns(2, geneset, equations, fnGenesToInputs)
+
+    def test_3_unknowns(self):
+        geneset = [i for i in range(-5, 5) if i != 0]
+
+        def fnGenesToInputs(genes):
+            return [
+                fractions.Fraction(genes[i], genes[i + 1])
+                for i in range(0, len(genes), 2)
+            ]
+
+        def e1(genes):
+            x, y, z = fnGenesToInputs(genes)
+            return 6 * x - 2 * y + 8 * z - 20
+
+        def e2(genes):
+            x, y, z = fnGenesToInputs(genes)
+            return y + 8 * x * z + 1
+
+        def e3(genes):
+            x, y, z = fnGenesToInputs(genes)
+            return (
+                2 * z * fractions.Fraction(6, x)
+                + 3 * fractions.Fraction(y, 2)
+                - 6
+            )
+
+        equations = [e1, e2, e3]
+        self.solve_unknowns(6, geneset, equations, fnGenesToInputs)
 
 
 if __name__ == "__main__":
