62dd516dc452960cf5a4845cd3dbc22880fa449c
Aleksander Los <oleklos@gmail.com>
2019-09-01 19:11:31 +0200

    Add test for four unknowns, add simulated anieling, add window

--- linearEquationTests.py
+++ linearEquationTests.py
@@ -1,8 +1,22 @@
 import unittest
 import datetime
 import fractions
+import random
 
 import genetic
+
+
+def mutate(genes, sortedGeneset, window, geneIndexes):
+    indexes = random.sample(geneIndexes, random.randint(1, len(genes))) \
+            if random.randint(0, 10) == 0 else [random.choice(geneIndexes)]
+    window.slide()
+    while len(indexes) > 0:
+        index = indexes.pop()
+        genesetIndex = sortedGeneset.index(genes[index])
+        start = max(0, genesetIndex - window.Size)
+        stop = min(len(sortedGeneset) - 1, genesetIndex + window.Size)
+        genesetIndex = random.randint(start, stop)
+        genes[index] = sortedGeneset[genesetIndex]
 
 
 def get_fitness(genes, equations):
@@ -12,7 +26,7 @@ def get_fitness(genes, equations):
 
 def display(candidate, startTime, fnGenesToInputs):
     timeDiff = datetime.datetime.now() - startTime
-    symbols = "xyz"
+    symbols = "xyza"
     result = ", ".join(
         "{} = {}".format(s, v)
         for s, v in zip(symbols, fnGenesToInputs(candidate.Genes))
@@ -31,9 +45,30 @@ class Fitness:
         return "diff: {:0.2f}".format(float(self.TotalDiffrence))
 
 
+class Window:
+    def __init__(self, minimum, maximum, size):
+        self.Min = minimum
+        self.Max = maximum
+        self.Size = size
+
+    def slide(self):
+        self.Size = self.Size - 1 \
+            if self.Size > self.Min else self.Max
+
+
 class LinearEquationTests(unittest.TestCase):
     def solve_unknowns(self, numUnknows, geneset, equations, fnGenesToInputs):
         startTime = datetime.datetime.now()
+        maxAge = 50
+        window = Window(max(1, int(len(geneset) / (2 * maxAge))),
+                        max(1, int(len(geneset) / 3)),
+                        int(len(geneset) / 2))
+
+        geneIndexes = [i for i in range(numUnknows)]
+        sortedGeneset = sorted(geneset)
+
+        def fnMutate(genes):
+            mutate(genes, sortedGeneset, window, geneIndexes)
 
         def fnDisplay(candidate):
             display(candidate, startTime, fnGenesToInputs)
@@ -48,7 +83,8 @@ class LinearEquationTests(unittest.TestCase):
             optimalFitness,
             geneset,
             fnDisplay,
-            maxAge=50,
+            fnMutate,
+            maxAge=maxAge,
         )
         self.assertTrue(not optimalFitness > best.Fitness)
 
@@ -96,6 +132,54 @@ class LinearEquationTests(unittest.TestCase):
 
         equations = [e1, e2, e3]
         self.solve_unknowns(6, geneset, equations, fnGenesToInputs)
+
+    def test_4_unknows(self):
+        geneRange = [i for i in range(-13, 13) if i != 0]
+        geneset = [i for i in set(
+            fractions.Fraction(d, e)
+            for d in geneRange
+            for e in geneRange if e != 0)]
+
+        def fnGenesToInputs(genes):
+            return genes
+
+        def e1(genes):
+            x, y, z, a = genes
+            return fractions.Fraction(1, 15) * x \
+                    - 2 * y \
+                    - 15 * z \
+                    - fractions.Fraction(4, 5) * a \
+                    - 3
+
+        def e2(genes):
+            x, y, z, a = genes
+            return -fractions.Fraction(5, 2) * x \
+                    - fractions.Fraction(9, 4) * y \
+                    + 12 * z \
+                    - a \
+                    - 17
+
+        def e3(genes):
+            x, y, z, a = genes
+            return -13 * x \
+                    + fractions.Fraction(3, 10) * y \
+                    - 6 * z \
+                    - fractions.Fraction(2, 5) * a \
+                    - 17
+
+        def e4(genes):
+            x, y, z, a = genes
+            return fractions.Fraction(1, 2) * x \
+                    + 2 * y \
+                    + fractions.Fraction(7, 4) * z \
+                    + fractions.Fraction(4, 3) * a \
+                    + 9
+
+        equations = [e1, e2, e3, e4]
+        self.solve_unknowns(4, geneset, equations, fnGenesToInputs)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(lambda: self.test_4_unknows())
 
 
 if __name__ == "__main__":
