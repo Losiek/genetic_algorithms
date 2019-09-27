6123f58c41faca1a5288d8540db5ad2f68af6a66
Aleksander Los <oleklos@gmail.com>
2019-09-27 16:36:03 +0200

    Add multimutation and benchmark

--- equationGenerationTests.py
+++ equationGenerationTests.py
@@ -45,26 +45,32 @@ def create(numbers, operations, minNumbers, maxNumbers):
     return genes
 
 
-def mutate(genes, numbers, operations, minNumbers, maxNumbers):
-    numberCount = (1 + len(genes)) / 2
-    appending = numberCount < maxNumbers and \
-                random.randint(0, 100) == 0
-    if appending:
-        genes.append(random.choice(operations))
-        genes.append(random.choice(numbers))
-        return
-    # Remove operation-number pair
-    removing = numberCount > minNumbers and \
-               random.randint(0, 20) == 0
-    if removing:
-        index = random.randrange(0, len(genes) - 1)
-        del genes[index]
-        del genes[index]
-        return
-    # And mutate an operation or number
-    index = random.randrange(0, len(genes))
-    genes[index] = random.choice(operations) \
-            if (index & 1) == 1 else random.choice(numbers)
+def mutate(genes, numbers, operations, minNumbers, maxNumbers, fnGetFitness):
+    count = random.randint(1, 10)
+    initialFitness = fnGetFitness(genes)
+    while count > 0:
+        count -= 1
+        if fnGetFitness(genes) > initialFitness:
+            return
+        numberCount = (1 + len(genes)) / 2
+        appending = numberCount < maxNumbers and \
+                    random.randint(0, 100) == 0
+        if appending:
+            genes.append(random.choice(operations))
+            genes.append(random.choice(numbers))
+            continue
+        # Remove operation-number pair
+        removing = numberCount > minNumbers and \
+                   random.randint(0, 20) == 0
+        if removing:
+            index = random.randrange(0, len(genes) - 1)
+            del genes[index]
+            del genes[index]
+            continue
+        # And mutate an operation or number
+        index = random.randrange(0, len(genes))
+        genes[index] = random.choice(operations) \
+                if (index & 1) == 1 else random.choice(numbers)
 
 
 def display(candidate, startTime):
@@ -111,10 +117,10 @@ class EquationGenerationTest(unittest.TestCase):
         optimalLengthSolution = [6, '^', 3, '*', 2, '-', 5]
         self.solve(operations, prioritizedOperations, optimalLengthSolution)
 
-    def solve(self, operations, prioritizedOperations, optimalLengthSolution):
+    def solve(self, operations, prioritizedOperations, optimalSolution):
         numbers = [1, 2, 3, 4, 5, 6, 7]
-        expectedTotal = evaluate(optimalLengthSolution, prioritizedOperations)
-        minNumbers = (1 + len(optimalLengthSolution)) / 2
+        expectedTotal = evaluate(optimalSolution, prioritizedOperations)
+        minNumbers = (1 + len(optimalSolution)) / 2
         maxNumbers = 6 * minNumbers
         startTime = datetime.datetime.now()
 
@@ -131,12 +137,16 @@ class EquationGenerationTest(unittest.TestCase):
             return create(numbers, operations, minNumbers, maxNumbers)
 
         def fnMutate(child):
-            mutate(child, numbers, operations, minNumbers, maxNumbers)
+            mutate(child, numbers, operations, minNumbers, maxNumbers,
+                   fnGetFitness)
 
-        optimalFitness = fnGetFitness(optimalLengthSolution)
+        optimalFitness = fnGetFitness(optimalSolution)
         best = genetic.get_best(fnGetFitness, None, optimalFitness, None,
                                 fnDisplay, fnMutate, fnCreate, maxAge=50)
         self.assertTrue(not optimalFitness > best.Fitness)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(lambda: self.test_exponent())
 
 
 if __name__ == '__main__':
