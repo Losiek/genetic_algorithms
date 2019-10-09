228951ff92645fd043a3d8b025864c51e3cec786
Aleksander Los <oleklos@gmail.com>
2019-10-09 16:21:16 +0200

    Fix XOR problem

--- circuitTests.py
+++ circuitTests.py
@@ -52,34 +52,44 @@ def display(candidate, startTime):
         timeDiff))
 
 
-def create_gene(index, geneset):
-    gateType = random.choice(geneset)
+def create_gene(index, gates, sources):
+    if index < len(sources):
+        gateType = sources[index]
+    else:
+        gateType = random.choice(gates)
     indexA = indexB = None
     if gateType[1].input_count() > 0:
         indexA = random.randint(0, index)
     if gateType[1].input_count() > 1:
-        indexB = random.randint(0, index) if index > 1 else 0
+        indexB = random.randint(0, index) \
+            if index > 1 and index >= len(sources) else 0
         if indexB == indexA:
             indexB = random.randint(0, index)
     return Node(gateType[0], indexA, indexB)
 
 
-def mutate(childGenes, fnCreateGene):
+def mutate(childGenes, fnCreateGene, fnGetFitness, sourceCount):
     count = random.randint(1, 5)
+    initialFitness = fnGetFitness(childGenes)
     while count > 0:
         count -= 1
-        indexesUsed = [i for i in nodes_to_circuit(childGenes)[1]]
+        indexesUsed = [i for i in nodes_to_circuit(childGenes)[1]
+                       if i >= sourceCount]
+        if len(indexesUsed) == 0:
+            return
         index = random.choice(indexesUsed)
         childGenes[index] = fnCreateGene(index)
+        if fnGetFitness(childGenes) > initialFitness:
+            return
 
 
 class CircuitTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.inputs = dict()
-        cls.geneset = [[circuits.And, circuits.And],
-                       [lambda i1, i2: circuits.Not(i1), circuits.Not],
-                       [lambda i1, i2: circuits.Source('A', cls.inputs), circuits.Source],
+        cls.gates = [[circuits.And, circuits.And],
+                     [lambda i1, i2: circuits.Not(i1), circuits.Not]]
+        cls.sources = [[lambda i1, i2: circuits.Source('A', cls.inputs), circuits.Source],
                        [lambda i1, i2: circuits.Source('B', cls.inputs), circuits.Source]]
 
     def test_generate_OR(self):
@@ -113,10 +123,10 @@ class CircuitTests(unittest.TestCase):
             display(candidate, startTime)
 
         def fnCreateGene(index):
-            return create_gene(index, self.geneset)
+            return create_gene(index, self.gates, self.sources)
 
         def fnMutate(genes):
-            mutate(genes, fnCreateGene)
+            mutate(genes, fnCreateGene, fnGetFitness, len(self.sources))
 
         def fnGetFitness(genes):
             return get_fitness(genes, rules, self.inputs)
